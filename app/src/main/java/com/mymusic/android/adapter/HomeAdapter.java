e9834dc67e0659d0c0fd7a4da31671c0dd909f33
cheng <1114638794@qq.com>
2018-08-26 15:33:32 +0800

    主页添加viewpager

---
+++ app/src/main/java/com/mymusic/android/adapter/HomeAdapter.java
@@ -0,0 +1,31 @@
+package com.mymusic.android.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.mymusic.android.fragment.MeFragment;
+import com.mymusic.android.fragment.MusicFragment;
+import com.mymusic.android.fragment.VideoFragment;
+
+
+/**
+ * Created by smile on 2018/5/26.
+ */
+
+public class HomeAdapter extends BaseFragmentPagerAdapter<Integer> {
+    public HomeAdapter(Context context, FragmentManager fm) {
+        super(context, fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return MeFragment.newInstance();
+        } else if (position == 1) {
+            return MusicFragment.newInstance();
+        } else {
+            return VideoFragment.newInstance();
+        }
+    }
+}
